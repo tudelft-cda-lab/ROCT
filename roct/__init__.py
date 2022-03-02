e96f4b3b6c554c64e27734c1e64144494b3bce19
Daniel Vos <daniel.vos.d@gmail.com>
2022-03-02 16:25:26 +0100

    Add roct pypi package

---
+++ roct/__init__.py
@@ -0,0 +1,3 @@
+from roct.maxsat import SATOptimalRobustTree
+from roct.milp import BinaryOptimalRobustTree, OptimalRobustTree
+from roct.upper_bound import maximum_adversarial_accuracy, samples_in_range, samples_in_range_linf
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="roct",
+    version="0.0.2",
+    author="Daniel Vos",
+    author_email="D.A.Vos@tudelft.nl",
+    url="https://github.com/tudelft-cda-lab/ROCT",
+    description="Robust Optimal Classification Trees",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["roct"],
+    install_requires=[
+        "numpy<=1.21",
+        "groot-trees",
+        "gurobipy",
+        "python-sat",
+        "tqdm",
+        "seaborn",
+    ]
+)
