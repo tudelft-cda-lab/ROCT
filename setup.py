f0747a97bbe92bb54ee81ec966f38b20004b8552
Daniel Vos <daniel.vos.d@gmail.com>
2022-03-07 16:16:21 +0100

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="roct",
-    version="0.0.2",
+    version="0.0.3",
     author="Daniel Vos",
     author_email="D.A.Vos@tudelft.nl",
     url="https://github.com/tudelft-cda-lab/ROCT",
